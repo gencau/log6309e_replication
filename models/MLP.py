bc0356956da5aa677660461406d6609c2760556f
Serp <manu.serpette74@gmail.com>
2023-10-12 20:00:30 -0400

    MLP made into class for manipulation

--- models/MLP.py
+++ models/MLP.py
@@ -9,57 +9,10 @@ import numpy as np
 print("Loading packages...")
 print(torch.__version__)
 print("done")
-# seq_level_data = np.load(
-#     '../logrep/BGL_MCV_agg.npz', allow_pickle=True)
-
-seq_level_data = np.load(
-    '../logrep/MCV_bglPP-sequential.npz.npz', allow_pickle=True)
-
-x_train = seq_level_data["x_train"]
-y_train = seq_level_data["y_train"]
-x_test = seq_level_data["x_test"]
-y_test = seq_level_data["y_test"]
-
-# balance pos/neg samples
-x_train = np.array(x_train, dtype=np.float64)
-x_test = np.array(x_test, dtype=np.float64)
-pos = y_train == 1
-print(x_train.shape)
-print(y_train.shape)
-print('positive:', sum(pos))
-print('ratio:', x_train.shape[0]/sum(pos))
-ratio = x_train.shape[0]/sum(pos)
-
-index = np.argwhere(pos == True)
-try:
-    fea_dim = x_train.shape[1]
-except:
-    fea_dim = 1
-
-pos_items = x_train[index, :].reshape(-1, fea_dim)
-pos_rep = np.repeat(pos_items, ratio-1, axis=0)
-
-new_x_train = np.concatenate((x_train, pos_rep), axis=0)
-new_y_train = np.append(y_train, np.ones(pos_rep.shape[0]))
-
-# shuffle new dataset
-
-index = np.arange(new_x_train.shape[0])
-np.random.shuffle(index)
-
-new_x_train_s = new_x_train[index]
-new_y_train_s = new_y_train[index]
-batch_size = y_train.shape[0]
 
 
-y_train_tensor = torch.from_numpy(new_y_train_s).type(torch.LongTensor)
-y_train_onehot = torch.from_numpy(new_y_train_s).type(torch.LongTensor)
-x_train_tensor = torch.from_numpy(new_x_train_s).float()
-
-x_test_tensor = torch.from_numpy(x_test).float()
-y_test_tensor = torch.from_numpy(y_test.reshape(-1, 1))
-y_test_onehot = torch.zeros(
-    y_test_tensor.shape[0], 2).scatter_(1, y_test_tensor, 1)
+def percentage(t, n):
+    return round(t/n*100, 2)
 
 
 class Net(torch.nn.Module):
@@ -78,24 +31,85 @@ class Net(torch.nn.Module):
         return out
 
 
-net = Net(fea_dim, 200, 2)
-print(net)
-optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
-loss_func = torch.nn.CrossEntropyLoss()
+class MLP_wrapper():
 
-for t in range(2000):
-    out = net(x_train_tensor)
-    loss = loss_func(out, y_train_onehot)
+    def __init__(self, name, n=2000, step=10):
+        self.n = n
+        self.name = name
+        self.step = step
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    if t % 10 == 0:
-        print('itr: %d, loss: %f' % (t, loss))
-        out_ = net(x_test_tensor)
-        y_ = torch.argmax(out_, -1)
-        acc = (y_ == torch.from_numpy(y_test)).sum()/y_.shape[0]
-        precision, recall, f1, _ = precision_recall_fscore_support(
-            y_test, y_, average='binary')
-        print('Testset: Precision: {:.3f}, recall: {:.3f}, F1-measure: {:.3f}, acc:{:.3f}\n'.format(
-            precision, recall, f1, acc))
+    def load_data(self, file):
+        print("Loading data "+file+" ...")
+        seq_level_data = np.load(file, allow_pickle=True)
+        self.x_train = seq_level_data["x_train"]
+        self.y_train = seq_level_data["y_train"]
+        self.x_test = seq_level_data["x_test"]
+        self.y_test = seq_level_data["y_test"]
+        print("done")
+        self.x_train = np.array(self.x_train, dtype=np.float64)
+        self.x_test = np.array(self.x_test, dtype=np.float64)
+        pos = self.y_train == 1
+        print(self.x_train.shape)
+        print(self.y_train.shape)
+        print('positive:', sum(pos))
+        print('ratio:', self.x_train.shape[0]/sum(pos))
+        ratio = self.x_train.shape[0]/sum(pos)
+        index = np.argwhere(pos == True)
+        self.fea_dim = self.x_train.shape[1]
+
+        pos_items = self.x_train[index, :].reshape(-1, self.fea_dim)
+        pos_rep = np.repeat(pos_items, ratio-1, axis=0)
+
+        new_x_train = np.concatenate((self.x_train, pos_rep), axis=0)
+        new_y_train = np.append(self.y_train, np.ones(pos_rep.shape[0]))
+        index = np.arange(new_x_train.shape[0])
+        np.random.shuffle(index)
+
+        new_x_train_s = new_x_train[index]
+        new_y_train_s = new_y_train[index]
+        batch_size = self.y_train.shape[0]
+
+        y_train_tensor = torch.from_numpy(new_y_train_s).type(torch.LongTensor)
+        self.y_train_onehot = torch.from_numpy(
+            new_y_train_s).type(torch.LongTensor)
+        self.x_train_tensor = torch.from_numpy(new_x_train_s).float()
+
+        self.x_test_tensor = torch.from_numpy(self.x_test).float()
+        y_test_tensor = torch.from_numpy(self.y_test.reshape(-1, 1))
+        self.y_test_onehot = torch.zeros(
+            y_test_tensor.shape[0], 2).scatter_(1, y_test_tensor, 1)
+
+    def train(self):
+        print("Training model "+self.name+" ...")
+        net = Net(self.fea_dim, 200, 2)
+        print(net)
+        optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+        loss_func = torch.nn.CrossEntropyLoss()
+        counter = 0
+        for t in range(self.n):
+            out = net(self.x_train_tensor)
+            loss = loss_func(out, self.y_train_onehot)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if percentage(t, self.n) > (counter+1)*self.step:
+                counter += 1
+                print('model %s itr: %d (%d), loss: %f' %
+                      (self.name, t, percentage(t, self.n), loss))
+                out_ = net(self.x_test_tensor)
+                y_ = torch.argmax(out_, -1)
+                self.acc = (y_ == torch.from_numpy(
+                    self.y_test)).sum()/y_.shape[0]
+                self.precision, self.recall, self.f1, _ = precision_recall_fscore_support(
+                    self.y_test, y_, average='binary')
+                print('Testset: Precision: {:.3f}, recall: {:.3f}, F1-measure: {:.3f}, acc:{:.3f}\n'.format(
+                    self.precision, self.recall, self.f1, self.acc))
+
+    def get_metrics(self):
+        return [self.precision, self.recall, self.f1, self.acc]
+
+
+model = MLP_wrapper("m1")
+model.load_data("../logrep/MCV_bglPP-sequential.npz.npz")
+model.train()
